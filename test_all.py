aaf2644e8d1d95a054effbfc1be4286ca46e1b77
hd702 <haru.doyu@gmail.com>
2024-02-13 07:53:07 +0200

    create mnist testing
    I implemented three tests on MNIST, comparing C inference with Python inference, which serves as a reference.
    
    1. Vanilla (non-quantized) Python vs. vanilla C.
    2. Vanilla (non-quantized) Python vs. quantized (group size 1) C.
    3. Vanilla (non-quantized) Python vs. quantized (group size 2) C.
    
    Tests 1 and 2 are expected to have only small numerical errors since they utilize the same parameters. Test 3 should have larger errors, albeit still close to the reference values due to small group size.

--- test_all.py
+++ test_all.py
@@ -1,3 +1,5 @@
+# Don't edit this file! This was automatically generated from "test_all.ipynb".
+
 """
 Run simply with
 $ pytest
@@ -6,33 +8,52 @@ $ pytest
 import os
 import subprocess
 import numpy as np
+import torch
+import struct
 
+from model import Model
+from export import export_modelq8
 
-def test_runc():
-    """ Test run.c file """
+def test_runfiles():
+    """ test run.c and runq.c works with an acceptable tolerance """
 
-    command = ["./run", "model.bin"]
-    with open('err.txt', mode='wb') as fe:
-        with open('stdout.txt', mode='wb') as fo:
+    file_path = "test/data/MNIST/sorted/7/1012"
+    
+    # run vanilla model in test mode
+    command = ["./run", "model.bin", file_path]
+    with open('err.txt', mode='w') as fe:
+        with open('stdout.txt', mode='w') as fo:
             proc = subprocess.Popen(command, stdout=fo, stderr=fe)
             proc.wait()
-    
-    inputs = [0.001, 1.57, 3.14, 6.28]
-    stdout = np.fromfile("stdout.txt", sep="\t")
-    assert np.allclose(stdout, np.sin([0.001, 1.57, 3.14, 6.28]), atol=0.1)
+    res = np.loadtxt("stdout.txt")
 
-
-def test_runq():
-    """ Test runq.c file """
-
-    command = ["./runq", "modelq8.bin"]
-    with open('errq.txt', mode='wb') as fe:
-        with open('stdoutq.txt', mode='wb') as fo:
+    # run quantized model test with group size = 1 in test mode
+    export_modelq8(file_path="modelq8_1.bin", gs=1)
+    command = ["./runq", "modelq8_1.bin", file_path]
+    with open('err1.txt', mode='w') as fe:
+        with open('stdout1.txt', mode='w') as fo:
             proc = subprocess.Popen(command, stdout=fo, stderr=fe)
             proc.wait()
-    
-    inputs = [0.001, 1.57, 3.14, 6.28]
-    stdout = np.fromfile("stdout.txt", sep="\t")
-    print(np.abs(stdout - np.sin([0.001, 1.57, 3.14, 6.28])))
-    assert np.allclose(stdout, np.sin([0.001, 1.57, 3.14, 6.28]), atol=0.1)
+    res1 = np.loadtxt("stdout1.txt")
+
+    # run quantized model test with group size = 1 in test mode
+    export_modelq8(file_path="modelq8_2.bin", gs=2)
+    command = ["./runq", "modelq8_2.bin", file_path]
+    with open('err2.txt', mode='w') as fe:
+        with open('stdout2.txt', mode='w') as fo:
+            proc = subprocess.Popen(command, stdout=fo, stderr=fe)
+            proc.wait()
+    res2 = np.loadtxt("stdout2.txt")
+
+    # retrieve reference values using Python model
+    model = torch.load("model.pt")
+    with open(file_path, "rb") as f:
+        image = torch.tensor(struct.unpack('B'*(28*28), f.read(28*28))).view(1,28,28)
+        image = ((image/255 - 0.5) / 0.5)
+        ref = model(image).detach()
+        ref = torch.nn.functional.softmax(ref, dim=1).view(-1).numpy() # python model output
+
+    assert np.allclose(res, ref, atol=1e-5, rtol=0), "run.c: Probabilities are not close."
+    assert np.allclose(res1, ref, atol=1e-5, rtol=0), "runq.c (group size = 1): Probabilities are not close."
+    assert np.allclose(res2, ref, atol=1e-2, rtol=0), "runq.c (group size = 2):Probabilities are not close."
 
