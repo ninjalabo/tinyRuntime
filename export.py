18f823c7bfe8ce28094b4a824abb5d4e1968a7cb
hd702 <haru.doyu@gmail.com>
2024-02-27 12:11:10 +0200

    write quantized CNN model
    File format is similar to export_model. I only added a few features: the number of parameters in the model, group sizes for each layer's weigths and biases, offsets for scaling factors and scaling factors to the end of file.
    
    The group size varies for different parameters, being calculated individually for each layer's weights and biases based on their respective sizes. By default, the group size is set to 64. If the dimension of specific parameters is less than 64, the group size will be set to that dimension. If the dimension is greater than 64 but not a multiple of 64, the group size will be set to the factor of the dimension that is closest to 64.

--- export.py
+++ export.py
@@ -3,6 +3,7 @@
 import torch
 import struct
 import numpy as np
+from sympy import divisors
 
 def serialize_fp32(file, tensor):
     ''' Write one fp32 tensor to file that is open in wb mode '''
@@ -44,9 +45,9 @@ def quantize_q80(w, group_size):
 
 def export_model(model, file_path="model.bin"):
     '''
-    Export the model to a file at the specified filepath
+    Export the model to a file
     The data inside the file follows this order:
-    1. Number of classes, CNN layers, and FC layers.
+    1. The number of classes, CNN layers, and FC layers.
     2. CNN and FC layers' configuration.
     3. CNN and FC layers' parameters.
     '''
@@ -64,11 +65,11 @@ def export_model(model, file_path="model.bin"):
         f.write(struct.pack("6i", layer.kernel_size[0], layer.stride[0], layer.padding[0],
                 layer.in_channels, layer.out_channels, offset))
         # set offset to the start of next layer
-        offset += layer.out_channels*layer.in_channels*layer.kernel_size[0]**2 + layer.out_channels
+        offset += layer.out_channels * layer.in_channels * layer.kernel_size[0]**2 + layer.out_channels
 
     for layer in linear_layers:
         f.write(struct.pack("3i", layer.in_features, layer.out_features, offset))
-        offset += layer.in_features*layer.out_features + layer.out_features
+        offset += layer.in_features * layer.out_features + layer.out_features
 
     # write the weights and biases of the model
     for layer in conv_layers:
@@ -84,30 +85,83 @@ def export_model(model, file_path="model.bin"):
     
     torch.save(model, "model.pt") # for loading in python
 
+def calculate_groupsize(dim, gs):
+    '''
+    Change the group size if dimension is smaller, and adjust if dim is not a
+    multiple of group size. Otherewise it remains the same.
+    '''
+    if dim < gs:
+        return dim
+    elif  dim % gs == 0:
+        return gs
+    else:
+        factors = list(divisors(dim)) # give the factors of number "dim"
+        return min(factors, key=lambda x: abs(x - gs)) # find the closest number to group size
+
 def export_modelq8(model_path="model.pt", file_path="modelq8.bin", gs=64):
-    ''' read a model from model.bin if not given and export a quatized (int8) model to filepath '''
+    '''
+    Export the quantized model to a file
+    The data inside the file follows this order:
+    1. The number of classes, CNN layers, and FC layers, parameters.
+    2. CNN and FC layers' configuration.
+    3. CNN and FC layers' quantized parameters.
+    4. CNN and FC layers' scaling factors.
+    '''
     model = torch.load(model_path)
     f = open(file_path, "wb")
-    # write the model structure 
-    header = struct.pack("iii", model.dim, model.nclasses, gs)
-    f.write(header) 
-    # quantize and write the model weights and biases
-    weights = [*[layer.weight for layer in model.layers], model.out.weight]
-    biases = [*[layer.bias for layer in model.layers], model.out.bias]
-    params = [*weights, *biases]
+    # write model config
+    conv_layers = [model.conv1, model.conv2]
+    nconv = len(conv_layers)
+    linear_layers = [model.fc1, model.fc2]
+    nlinear = len(linear_layers)
+    nparameters = sum(p.numel() for p in model.parameters())
+    header = struct.pack("4i", model.nclasses, nconv, nlinear, nparameters)
+    f.write(header)
+    # write layers' config
+    qoffset = 0 # offset for quantized parameters
+    soffset = 0 # offset for scaling factors
+    group_sizes = [] # save group sizes of each layer
+    for layer in conv_layers:
+        # calculates group sizes for weights and biases
+        gs_weight = calculate_groupsize(layer.in_channels * layer.kernel_size[0]**2, gs)
+        gs_bias = calculate_groupsize(layer.out_channels, gs)
+        group_sizes.append(gs_weight)
+        group_sizes.append(gs_bias)
+
+        f.write(struct.pack("9i", layer.kernel_size[0], layer.stride[0], layer.padding[0],
+                layer.in_channels, layer.out_channels, qoffset, soffset, gs_weight, gs_bias))
+        # set offsets to the start of next layer
+        nweights = layer.out_channels * layer.in_channels * layer.kernel_size[0]**2
+        qoffset += nweights + layer.out_channels
+        soffset += nweights // gs_weight + layer.out_channels // gs_bias
+
+    for layer in linear_layers:
+        gs_weight = calculate_groupsize(layer.in_features, gs)
+        gs_bias = calculate_groupsize(layer.out_features, gs)
+        group_sizes.append(gs_weight)
+        group_sizes.append(gs_bias)
+
+        f.write(struct.pack("6i", layer.in_features, layer.out_features, qoffset, soffset,
+                            gs_weight, gs_bias))
+
+        nweights = layer.in_features * layer.out_features
+        qoffset += nweights + layer.out_features
+        soffset += nweights // gs_weight + layer.out_features // gs_bias
 
     ew = []
-    for i, p in enumerate(params):
-        if i==len(params)-1 and gs>model.nclasses:
-            gs = model.nclasses
-        # quantize this weight
-        q, s, err = quantize_q80(p, gs)
-        # save the int8 weights to file
-        serialize_int8(f, q) # save the tensor in int8
+    scaling_factors = []
+    i = 0
+    for layer in [*conv_layers, *linear_layers]:
+        for p in layer.parameters():
+            q, s, err = quantize_q80(p, group_sizes[i])
+            serialize_int8(f, q) # save the tensor in int8
+            scaling_factors.append(s)
+            ew.append((err, p.shape))
+            i += 1
+            print(f"Quantized {tuple(p.shape)} to Q8_0 with max error {err}")
+
+    for s in scaling_factors:
         serialize_fp32(f, s) # save scale factors
-        # logging
-        ew.append((err, p.shape))
-        print(f"{i+1}/{len(params)} quantized {tuple(p.shape)} to Q8_0 with max error {err}")
 
     # print the highest error across all parameters, should be very small, e.g. O(~0.001)
     ew.sort(reverse=True)
