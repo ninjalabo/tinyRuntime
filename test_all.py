18b5acb0a07fb76db678e9ad342ec2740ccf9731
hd702 <haru.doyu@gmail.com>
2024-02-29 08:25:27 +0200

    add workflow artifacts property to store outputs in workflow test

--- test_all.py
+++ test_all.py
@@ -18,32 +18,39 @@ def test_runfiles():
     """ test run.c and runq.c works with an acceptable tolerance """
 
     file_path = "test/data/MNIST/sorted/7/1012"
-    
+    d = "test_outputs"
+    os.makedirs(d, exist_ok=True)
     # run vanilla model in test mode
+    err = os.path.join(d, 'err.txt')
+    stdout = os.path.join(d, 'stdout.txt')
     command = ["./run", "model.bin", file_path]
-    with open('err.txt', mode='w') as fe:
-        with open('stdout.txt', mode='w') as fo:
+    with open(err, mode='w') as fe:
+        with open(stdout, mode='w') as fo:
             proc = subprocess.Popen(command, stdout=fo, stderr=fe)
             proc.wait()
-    res = np.loadtxt("stdout.txt")
+    res = np.loadtxt(stdout)
 
     # run quantized model test with group size = 1 in test mode
+    err1 = os.path.join(d, 'err1.txt')
+    stdout1 = os.path.join(d, 'stdout1.txt')
     export_modelq8(file_path="modelq8_1.bin", gs=1)
     command = ["./runq", "modelq8_1.bin", file_path]
-    with open('err1.txt', mode='w') as fe:
-        with open('stdout1.txt', mode='w') as fo:
+    with open(err1, mode='w') as fe:
+        with open(stdout1, mode='w') as fo:
             proc = subprocess.Popen(command, stdout=fo, stderr=fe)
             proc.wait()
-    res1 = np.loadtxt("stdout1.txt")
+    res1 = np.loadtxt(stdout1)
 
     # run quantized model test with group size = 1 in test mode
+    err2 = os.path.join(d, 'err2.txt')
+    stdout2 = os.path.join(d, 'stdout2.txt')
     export_modelq8(file_path="modelq8_2.bin", gs=2)
     command = ["./runq", "modelq8_2.bin", file_path]
-    with open('err2.txt', mode='w') as fe:
-        with open('stdout2.txt', mode='w') as fo:
+    with open(err2, mode='w') as fe:
+        with open(stdout2, mode='w') as fo:
             proc = subprocess.Popen(command, stdout=fo, stderr=fe)
             proc.wait()
-    res2 = np.loadtxt("stdout2.txt")
+    res2 = np.loadtxt(stdout2)
 
     # retrieve reference values using Python model
     model = torch.load("model.pt")
