9c3846aa16cdb34314ba8320ed1c033221f3251a
hd702 <haru.doyu@gmail.com>
2024-03-13 15:52:11 +0200

    resize Imagenette dataset and save them to binary files
    To compare the execution time of Python and C inference without data processing, we preprocess images beforehand. In the notebook, the Imagenette dataset is first downloaded in its full size. Then, each image in the test set is resized to the shape (3, 224, 224) and saved to binary files under `data/val_transformed/<label>`, where `<label>` represents the class of the image. Additionally, the script `prep.ipynb` or `prep.py` creates the directory `data/val_transformed_subset`, which contains a subset (10 images from each class) of the images stored in `data/val_transformed`, facilitating quick testing.

--- prep.py
+++ prep.py
@@ -1,37 +1,60 @@
 # Don't edit this file! This was automatically generated from "prep.ipynb".
 
 import os
+import shutil
 import torchvision
+from torchvision.datasets.utils import download_and_extract_archive
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
 
-torchvision.datasets.MNIST(root='./data', train=False, download=True)
+from export import serialize_fp32
 
-base_path = './data/MNIST/raw/'
-os.listdir(base_path)
+base = "data"
+os.makedirs(base, exist_ok=True)
 
-path = os.path.join(base_path, 't10k-labels-idx1-ubyte')
-print(os.stat(path).st_size)
-with open(path, 'rb') as f:
-    labels = list(f.read())
+url = "https://s3.amazonaws.com/fast-ai-imageclas/imagenette2.tgz"
+download_and_extract_archive(url, download_root=base, extract_root=base)
 
-len(labels), labels[:12]
+# resize images into (3, 224, 224) and normalize
+transform = transforms.Compose([transforms.Resize((224, 224)),
+                                transforms.ToTensor()])
+dataset = ImageFolder(root="data/imagenette2/val", transform=transform)
 
-path = os.path.join('./data/MNIST', 'sorted')
-os.makedirs(path, exist_ok=True)
-for i in range(10):
-    p = os.path.join(path, str(i))
-    os.makedirs(p, exist_ok=True)
-sorted(os.listdir(path))
+# save transformed images to binary files
+dst = "data/imagenette2/val_transformed"
+os.makedirs(dst, exist_ok=True)
 
-labels = labels[8:]
-labels = [(i, l) for i, l in enumerate(labels)]
-labels[:9]
+for i, (image, label) in enumerate(dataset):
+    if i % 500 == 0:
+        print(f"Wrote {i}/{len(dataset)}")
 
-with open('./data/MNIST/raw/t10k-images-idx3-ubyte', 'rb') as f1:
-    for i, l in labels:
-        path = os.path.join('./data/MNIST/sorted', str(l), str(i))
-        with open(path, 'wb') as f2:
-            f1.seek(16+28*28*i, 0)
-            x = f1.read(28*28)
-            #show_image(x)
-            f2.write(x)
-            #break
+    dsd = os.path.join(dst, str(int(label)))
+    os.makedirs(dsd, exist_ok=True)
+    df = os.path.join(dsd, str(i))
+    f = open(df, "wb")
+    serialize_fp32(f, image)
+    f.close()
+print("Done")
+
+# create directory containing the subset of `val_transformed`
+src = "data/imagenette2/val_transformed"
+dst = "data/imagenette2/val_transformed_subset"
+os.makedirs(dest, exist_ok=True)
+seed = 4
+nsamples = 10
+
+for label in range(10):
+    ssd = os.path.join(src, str(label))
+    dsd = os.path.join(dst, str(label))
+    os.makedirs(dsd, exist_ok=True)
+
+    # shuffle image files
+    files = os.listdir(ssd)
+    random.Random(seed).shuffle(files)
+
+    # copy 10 samples to `val_transformed_subset`
+    for f in files[:nsamples]:
+        sf = os.path.join(ssd, f)
+        df = os.path.join(dsd, f)
+        shutil.copy(sf, df)
