8c253c856758ca2b4c72069a7dccce4c9c83794d
Juho Kokko <85553149+coqelz@users.noreply.github.com>
2024-03-13 13:56:49 +0200

    merged training and model

--- model.py
+++ model.py
@@ -2,27 +2,97 @@
 
 import torch
 from torch import nn
+import numpy as np
 
-class Model(nn.Module):
-    def __init__(self):
-        super(Model, self).__init__()
-        self.nclasses = 10
-
-        self.conv1 = nn.Conv2d(1, 4, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(4, 8, kernel_size=3, padding=1)
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
-
-        self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(8*7*7, 128)
-        self.fc2 = nn.Linear(128, self.nclasses)
-        self.relu = nn.ReLU()
+# one block with skip connection
+class Block(nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1):
+        """
+        Args:
+          in_channels (int):  Number of input channels.
+          out_channels (int): Number of output channels.
+          stride (int):       Controls the stride.
+        """
+        super(Block, self).__init__()
         
-    def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.pool(x)
-        x = self.pool(self.relu(self.conv2(x)))
-        x = self.flatten(x)
-        x = self.relu(self.fc1(x))
-        x = self.fc2(x)
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, stride=stride, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, 3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels)
+        )
+        
+        if stride != 1 or in_channels != out_channels:
+            self.skip_connection = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, 1, stride=stride, bias=False),
+                nn.BatchNorm2d(out_channels)
+            )
+        else:
+            self.skip_connection = nn.Identity()
+        
+        self.act = nn.ReLU()
 
-        return x
+    def forward(self, x):
+        y = self.block(x)
+        y += self.skip_connection(x)
+        y = self.act(y)
+        return y
+
+# group of blocks
+class GroupOfBlocks(nn.Module):
+    def __init__(self, in_channels, out_channels, n_blocks, stride=1):
+        super(GroupOfBlocks, self).__init__()
+
+        first_block = Block(in_channels, out_channels, stride)
+        other_blocks = [Block(out_channels, out_channels) for _ in range(1, n_blocks)]
+        self.group = nn.Sequential(first_block, *other_blocks)
+
+    def forward(self, x):
+        return self.group(x)
+
+class ResNetImagenette(nn.Module):
+  def __init__(self, n_blocks=2, n_channels=64, n_class=10):
+    super(ResNetImagenette, self).__init__()
+    self.conv1 = nn.Conv2d(in_channels=3, out_channels=n_channels, kernel_size=7, stride=1, padding=2, bias=False)
+    self.bn1 = nn.BatchNorm2d(n_channels)
+    self.act = nn.ReLU(inplace=True)
+    self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+
+    self.group1 = GroupOfBlocks(n_channels, n_channels, n_blocks)
+    self.group2 = GroupOfBlocks(n_channels, 2*n_channels, n_blocks, stride=2)
+    self.group3 = GroupOfBlocks(2*n_channels, 4*n_channels, n_blocks, stride=2)
+    self.group4 = GroupOfBlocks(4*n_channels, 8*n_channels, n_blocks, stride=2)
+
+    self.avgpool = nn.AdaptiveAvgPool2d(output_size=(1, 1))
+    self.fc = nn.Linear(8*n_channels, n_class)
+
+    # Initialize weights
+    for m in self.modules():
+        if isinstance(m, nn.Conv2d):
+            n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+            m.weight.data.normal_(0, np.sqrt(2. / n))
+        elif isinstance(m, nn.BatchNorm2d):
+            m.weight.data.fill_(1)
+            m.bias.data.zero_()
+
+  def forward(self, x):
+
+    # first conv layer
+    x = self.conv1(x)
+    x = self.bn1(x)
+    x = self.act(x)
+    x = self.maxpool(x)
+
+    # residual blocks
+    x = self.group1(x)
+    x = self.group2(x)
+    x = self.group3(x)
+    x = self.group4(x)
+
+    # final
+    x = self.avgpool(x)
+    x = x.view(-1, self.fc.in_features)
+    x = self.fc(x)
+
+    return x
--- train.py
+++ train.py
@@ -1,17 +1,50 @@
 # Don't edit this file! This was automatically generated from "train.ipynb".
 
-import torchvision
-from torchvision import transforms
+import numpy as np
+import torch
 from torch import nn
+import matplotlib.pyplot as plt
+import struct
+import os
+
+import torchvision.models as models
+from torchvision.models import ResNet18_Weights
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("CUDA (GPU) is available.")
+else:
+    device = torch.device("cpu")
+device
+
+import torchvision
+from torchvision.datasets.utils import download_and_extract_archive
+
+dir_path = "data"
+os.makedirs(dir_path, exist_ok=True)
+
+# URL for Imagenette (Full size)
+url = "https://s3.amazonaws.com/fast-ai-imageclas/imagenette2.tgz"
+
+# Download and extract the dataset
+download_and_extract_archive(url, download_root=dir_path, extract_root=dir_path)
 
 def generate_dataloader(batch_size=32):
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+    transform = transforms.Compose([
+        transforms.Resize((224, 224)),  # Resize the image to fit ResNet input size
+        transforms.ToTensor(),
+    ])
     
-    trainset = torchvision.datasets.MNIST("./data", train=True, download=True, transform=transform)
-    trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
-    
-    testset = torchvision.datasets.MNIST("./data", train=False, download=True, transform=transform)
-    testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False)
+    train_dataset = ImageFolder(root=f"data/imagenette2/train", transform=transform)
+    test_dataset = ImageFolder(root=f"data/imagenette2/val", transform=transform)
+
+    trainloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
+                             num_workers=torch.get_num_threads())
+    testloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False,
+                            num_workers=torch.get_num_threads())
     
     return trainloader, testloader
 
@@ -21,12 +54,17 @@ def test_model(model, testloader):
     with torch.no_grad():
         vloss = 0.
         correct = 0.
-        for X,y in testloader:
-            out = model(X)
-            vloss += loss_fn(out, y).item()
-            correct += (torch.argmax(out, 1)==y).float().sum().item()
+
+        # Fetch the first batch
+        for inputs, targets in testloader:
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = model(inputs)
+            vloss += loss_fn(outputs, targets).item()
+            _, predicted = torch.max(outputs.data, 1)
+            correct += (predicted==targets).sum().item()
     
     return vloss/len(testloader),  correct/len(testloader.dataset)
+
 
 def train_model(model):  
     # training
@@ -38,10 +76,11 @@ def train_model(model):
 
         model.train()
         tloss = 0
-        for X,y in trainloader:
+        for inputs, targets in trainloader:
+            inputs, targets = inputs.to(device), targets.to(device)
             opt.zero_grad()
-            out = model(X)
-            loss = loss_fn(out, y)
+            out = model(inputs)
+            loss = loss_fn(out, targets)
             loss.backward()
             tloss += loss.item()
             opt.step()
@@ -50,3 +89,4 @@ def train_model(model):
         vloss, correct = test_model(model, testloader)
 
         print('LOSS train {} valid {} accuracy {:.5f}'.format(tloss, vloss, correct))
+    torch.save(model, "model.pt")
