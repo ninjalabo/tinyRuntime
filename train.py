43ab9cd6e625b835b160d8e5a827d0c2e681926b
Juho Kokko <85553149+coqelz@users.noreply.github.com>
2024-03-27 13:25:14 +0200

    explored fastai model implementation

--- train.py
+++ train.py
@@ -1,65 +1,43 @@
 # Don't edit this file! This was automatically generated from "train.ipynb".
 
+from fastai.vision.all import *
 import torch
-from torch import nn
-import torchvision.transforms as transforms
-from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader
-
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-def generate_dataloader(batch_size=32):
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),  # Resize the image to fit ResNet input size
-        transforms.ToTensor(),
-    ])
 
-    train_dataset = ImageFolder(root=f"data/imagenette2/train", transform=transform)
-    test_dataset = ImageFolder(root=f"data/imagenette2/val", transform=transform)
+#path = untar_data(URLs.IMAGENETTE) # this downloads to ~/.fastai/data/imagenette2
+models = {
+    'resnet18': resnet18,
+    'resnet34': resnet34,
+    'resnet50': resnet50,
+}
+def train(model_name, epochs=1):
+    if model_name not in models:
+        raise ValueError(f"Model name '{model_name}' is not supported. Choose from: {list(models.keys())}")
+ 
+    model = models[model_name]
 
-    trainloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
-                             num_workers=torch.get_num_threads())
-    testloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False,
-                            num_workers=torch.get_num_threads())
+    path = Path('data/imagenette2')
+    # ensure that transformed data is not used as well
+    train_fnames = get_image_files(path/'train')
+    val_fnames = get_image_files(path/'val')
+    fnames = train_fnames + val_fnames
 
-    return trainloader, testloader
+    # load the data
+    dls = ImageDataLoaders.from_path_func(
+        path, fnames, label_func=parent_label, item_tfms=Resize(224))
+    
+    # setup model with pretrained weights and data
+    learn = vision_learner(dls, model, metrics=accuracy)
 
-def test_model(model, testloader):
-    loss_fn = nn.CrossEntropyLoss()
-    model.eval()
-    with torch.no_grad():
-        vloss = 0.
-        correct = 0.
+    # train with model frozen except for the last layer
+    learn.fine_tune(epochs)
 
-        # Fetch the first batch
-        for inputs, targets in testloader:
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = model(inputs)
-            vloss += loss_fn(outputs, targets).item()
-            _, predicted = torch.max(outputs.data, 1)
-            correct += (predicted==targets).sum().item()
+    # save the model to models folder
+    save_path = f'models/{model_name}.pt'
+    torch.save(learn.model, save_path)
 
-    return vloss/len(testloader),  correct/len(testloader.dataset)
-
-def train_model(model):  
-    # training
-    loss_fn = nn.CrossEntropyLoss()
-    opt = torch.optim.AdamW(model.parameters(), lr=0.001)
-    trainloader, testloader = generate_dataloader()
-    for epoch in range(1):
-
-        model.train()
-        tloss = 0
-        for inputs, targets in trainloader:
-            inputs, targets = inputs.to(device), targets.to(device)
-            opt.zero_grad()
-            out = model(inputs)
-            loss = loss_fn(out, targets)
-            loss.backward()
-            tloss += loss.item()
-            opt.step()
-        tloss = tloss/len(trainloader)
-        vloss, correct = test_model(model, testloader)
-
-        print('LOSS train {} valid {} accuracy {:.5f}'.format(tloss, vloss, correct))
-    torch.save(model, "model.pt")
+def load(model_name):
+    model = torch.load(f'models/{model_name}.pt')
+    model.to(device)
+    return model
