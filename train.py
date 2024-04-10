7c39bf0d4a815e6f2fbb8ff135944715fc169566
Juho Kokko <85553149+coqelz@users.noreply.github.com>
2024-04-10 09:37:27 +0300

    add fastai to train the ResNet models with pretrained weights. The models are stored to huggingface and can be loaded there. A function 'load' has been made for easier model loading using just the model name.

--- train.py
+++ train.py
@@ -1,65 +1,34 @@
 # Don't edit this file! This was automatically generated from "train.ipynb".
 
-import torch
-from torch import nn
-import torchvision.transforms as transforms
-from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader
+from fastai.vision.all import *
+from huggingface_hub import from_pretrained_fastai, push_to_hub_fastai
+repo ='jkokko/'
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+models = {
+    'resnet18': resnet18,
+    'resnet34': resnet34,
+    'resnet50': resnet50,
+}
 
-def generate_dataloader(batch_size=32):
-    transform = transforms.Compose([
-        transforms.Resize((224, 224)),  # Resize the image to fit ResNet input size
-        transforms.ToTensor(),
-    ])
+def train(model_name, epochs=1):
+    if model_name not in models:
+        raise ValueError(f"Model name '{model_name}' is not supported. Choose from: {list(models.keys())}")
+ 
+    model = models[model_name]
+    path = untar_data(URLs.IMAGENETTE_320,data=Path.cwd()/'data')
+    dls = ImageDataLoaders.from_folder(path, valid='val', item_tfms=Resize(224), batch_tfms=Normalize.from_stats(*imagenet_stats),)
+    learn = vision_learner(dls, model, metrics=accuracy, pretrained=True)
+    learn.fine_tune(epochs)
 
-    train_dataset = ImageFolder(root=f"data/imagenette2/train", transform=transform)
-    test_dataset = ImageFolder(root=f"data/imagenette2/val", transform=transform)
+    return learn
 
-    trainloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True,
-                             num_workers=torch.get_num_threads())
-    testloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False,
-                            num_workers=torch.get_num_threads())
+def train_all(epochs=1):
+    for model_name in models:
+        learn = train(model_name, epochs)
+        push_to_hub_fastai(learner=learn, repo_id=repo + model_name)
+        
 
-    return trainloader, testloader
 
-def test_model(model, testloader):
-    loss_fn = nn.CrossEntropyLoss()
-    model.eval()
-    with torch.no_grad():
-        vloss = 0.
-        correct = 0.
-
-        # Fetch the first batch
-        for inputs, targets in testloader:
-            inputs, targets = inputs.to(device), targets.to(device)
-            outputs = model(inputs)
-            vloss += loss_fn(outputs, targets).item()
-            _, predicted = torch.max(outputs.data, 1)
-            correct += (predicted==targets).sum().item()
-
-    return vloss/len(testloader),  correct/len(testloader.dataset)
-
-def train_model(model):  
-    # training
-    loss_fn = nn.CrossEntropyLoss()
-    opt = torch.optim.AdamW(model.parameters(), lr=0.001)
-    trainloader, testloader = generate_dataloader()
-    for epoch in range(1):
-
-        model.train()
-        tloss = 0
-        for inputs, targets in trainloader:
-            inputs, targets = inputs.to(device), targets.to(device)
-            opt.zero_grad()
-            out = model(inputs)
-            loss = loss_fn(out, targets)
-            loss.backward()
-            tloss += loss.item()
-            opt.step()
-        tloss = tloss/len(trainloader)
-        vloss, correct = test_model(model, testloader)
-
-        print('LOSS train {} valid {} accuracy {:.5f}'.format(tloss, vloss, correct))
-    torch.save(model, "model.pt")
+def load(model_name):
+    learn = from_pretrained_fastai(repo_id=repo + model_name)
+    return learn
