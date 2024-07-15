f3ec22b003c8da933c385ce48f7fd254deda03b5
hd702 <haru.doyu@gmail.com>
2024-07-15 05:45:25 +0300

    update benchmarking
    Every time any C file is modified, GitHub Actions will test the accuracy, execution time, memory usage and model size across six runs in total: on two platforms (x86 and ARM) and with three runtimes (PyTorch, TinyRuntime, and Quantized TinyRuntime) -> 2 platforms * 3 runtimes = 6 combinations. These tests are executed by `benchmark.py` and the results are saved to `benchmark.csv`, which is subsequently pushed to https://github.com/ninjalabo/ninjalabo.github.io/tree/main. Based on these results, visualization is performed and displayed on our website.

--- benchmark.py
+++ benchmark.py
@@ -1,22 +1,108 @@
-import pytest
+# Don't edit this file! This was automatically generated from "benchmark.ipynb".
+
 import subprocess
-from train import load
+import os
+import time
+import psutil
+import struct
+import numpy as np
+import torch
+import csv
+from datetime import datetime
+import platform
+
 from export import export_model, export_modelq8
+from train import load
 
+def run_c(dir_path, base_command, model_path):
+    # get file paths of images and their labels
+    files = []
+    for label in range(10):
+        sd_path = os.path.join(dir_path, str(label))
+        f_paths = [os.path.join(sd_path, file) for file in os.listdir(sd_path)]
+        files += f_paths
+
+    # run C inference
+    command = [base_command, model_path, *files]
+    mems = []
+
+    start_time = time.time()
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = psutil.Process(process.pid)
+    while process.poll() == None:
+        try:
+            # `proc.memory_info().rss` returns the physical memory the process has used
+            mems.append(proc.memory_info().rss / (1024 * 1024)) # append in megabytes
+            time.sleep(0.1)  # check memory usage every 0.1 second
+        except psutil.NoSuchProcess: # handle the case where the process ends abruptly
+            pass
+    end_time = time.time()
+
+    output, _ = process.communicate()
+    acc = float(output.decode().strip())
+    dur = end_time - start_time
+    model_size = os.path.getsize(model_path) / (1024 * 1024)
+
+    return acc, dur, model_size, mems
+
+
+def run_python(dir_path, model_path):
+    command = ["python", "run.py", model_path, dir_path]
+    mems = []
+
+    start_time = time.time()
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc = psutil.Process(process.pid)
+    while process.poll() == None:
+        try:
+            # `proc.memory_info().rss` returns the physical memory the process has used
+            mems.append(proc.memory_info().rss / (1024 * 1024)) # append in megabytes
+            time.sleep(0.1)  # check memory usage every 0.1 second
+        except psutil.NoSuchProcess: # handle the case where the process ends abruptly
+            pass
+    end_time = time.time()
+
+    output, _ = process.communicate()
+    acc = float(output.decode().strip())
+    dur = end_time - start_time
+    model_size = os.path.getsize(model_path) / (1024 * 1024)
+
+    return acc, dur, model_size, mems
+
+# Generate model files for tinyRuntime
+path = "data"
 model = load("resnet18").model
-export_model(model, "resnet18.bin")
-export_modelq8(model, "resnet18-q8.bin")
+export_model(model, "model.bin")
+export_modelq8(model, "model-q8.bin")
 
-def run_vanilla():
-    command = ["./run", "resnet18.bin", "data/*/*"]
-    return subprocess.run(command)
+def save_benchmark_csv():
+    # Get results
+    commit_id = os.getenv('GITHUB_SHA')
+    time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    architecture = platform.machine()
+    res0 = run_python(path, "model.pkl")
+    res1 = run_c(path, "./run", "model.bin")
+    res2 = run_c(path, "./runq", "model-q8.bin")
 
-def run_quantized():
-    command = ["./runq", "resnet18-q8.bin", "data/*/*"]
-    return subprocess.run(command)
+    def generate_dict(res, runtime, quantization=False):
+        d = {"Commit": commit_id, "Datetime": time, "Architecture": architecture, "Runtime": runtime,
+             "Quantization": quantization, "Accuracy": res[0], "Time": res[1], "Model size": res[2],
+             "Max memory": np.max(res[3])}
+        return d
 
-def test_vanilla_tinyruntime(benchmark):
-    benchmark(run_vanilla)
+    data = [generate_dict(res0, "PyTorch"), generate_dict(res1, "tinyRuntime"),
+            generate_dict(res2, "tinyRuntime", quantization=True)]
 
-def test_quantized_tinyruntime(benchmark):
-    benchmark(run_quantized)
+    # Write results
+    csv_file = "benchmark.csv"
+    with open(csv_file, 'a', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=data[0].keys())
+        # If the file is empty, write header
+        if f.tell() == 0:
+            writer.writeheader()
+        for row in data:
+            writer.writerow(row)
+
+    print(f'Data has been appended to {csv_file}.')
+
+save_benchmark_csv()
