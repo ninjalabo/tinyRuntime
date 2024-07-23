7676a93c36aa78810d9294655e857e7e8bc33bde
hd702 <haru.doyu@gmail.com>
2024-07-23 05:13:57 +0300

    fix compiler to load model to CPU
    `ninjalabo/compiler-export-models` (`prep_model.py`), which previously loaded the model on the device where it was saved, now always loads the model to the CPU. This change avoids errors when loading a GPU-saved model on a CPU-only machine. Additionally, the GitHub workflow for the compiler has been modified to be triggered only when related files are changed.

--- prep_model.py
+++ prep_model.py
@@ -7,7 +7,7 @@ with open("md/runtime_info.json", "r") as f:
     runtime_info = json.load(f)
 quantization = runtime_info.get("compression", {}).get("quantization", False)
 
-model = torch.load("md/model.pkl")
+model = torch.load("md/model.pkl", map_location="cpu")
 
 export_function = export_modelq8 if quantization else export_model
 export_function(model, "md/model.bin")
